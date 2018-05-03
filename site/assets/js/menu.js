fe5f804503d22db4def6c7fcce984f622fc68e68
Patrick Monteiro <eng.patrickmonteiro@gmail.com>
2018-05-02 23:00:45 -0300

    compressao nas imagens, melhoria na utilização sem jquery no menu

--- site/assets/js/menu.js
+++ site/assets/js/menu.js
@@ -1,12 +1,26 @@
-$(window).on('load', function() {
-	var menu = $('.header-menu');
+// $(window).on('load', function() {
+// 	var menu = $('.header-menu');
 
-	$('.header-menu-titulo').on('click', function() {
-		if (menu.is('[data-ativo]')) {
-			menu.removeAttr('data-ativo');
-		} else {
-			menu.attr('data-ativo', '');
-		}
-	});
+// 	$('.header-menu-titulo').on('click', function() {
+// 		if (menu.is('[data-ativo]')) {
+// 			menu.removeAttr('data-ativo');
+// 		} else {
+// 			menu.attr('data-ativo', '');
+// 		}
+// 	});
 
+// });
+window.addEventListener('load', function() {
+	var titulo = document.querySelector('.header-menu-titulo');
+	var menu = document.querySelector('.header-menu');
+
+	if (titulo) {
+			titulo.onclick = function() {
+					if (menu.hasAttribute('data-ativo')) {
+							menu.removeAttribute('data-ativo');
+					} else {
+							menu.setAttribute('data-ativo', '');
+					}
+			};
+	}
 });
